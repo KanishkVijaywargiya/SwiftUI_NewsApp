4105251429f140765050179527aa4121412a2cb2
Kanishk Vijaywargiya <secretcodersociety@gmail.com>
2021-09-06 14:42:32 +0530

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by KANISHK VIJAYWARGIYA on 06/09/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
